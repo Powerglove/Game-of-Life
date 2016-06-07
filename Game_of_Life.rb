88c6760a51c943a69818f420ed8a7f4a18420b1c
Powerglove <lang.joh.jl@gmail.com>
2016-06-07 21:52:36 +0200

    add Game class and tests

--- Game_of_Life.rb
+++ Game_of_Life.rb
@@ -6,28 +6,29 @@ class Grid
 	HEIGHT = 10
 
 	def initialize
-	@cells = Array.new(WIDTH) { Array.new(HEIGHT, " ") }
+	@cells = Array.new(WIDTH) { Array.new(HEIGHT, (false)) }
 	end
+
+  def set_cell(x, y, dead_or_alive)
+    @cells[x][y] = dead_or_alive
+  end
 
 	def create_and_set_position
     35.times do
       x = rand(0..9)
       y = rand(0..9)
-      set_cell(x, y, "X")
+      set_cell(x, y, true)
     end
   end
 
-	def set_cell(x, y, live)
-    @cells[x][y] = live
-  end
-
-  def current_cell(x, y)
+  def live?(x, y)
     @cells[x][y]
   end
 
   def play_game
     create_and_set_position
     board_as_string
+    print_board
   end
 
   def print_board
@@ -36,9 +37,14 @@ class Grid
 
   def board_as_string
     board = ""
-    @cells.each do |x|
-      x.each do |y|
-        board << y
+
+    HEIGHT.times do |y|
+      WIDTH.times do |x|
+        if live?(x,y)
+          board << 'X'
+        else
+          board << ' '
+        end
       end
       board << "\n"
     end
@@ -46,14 +52,88 @@ class Grid
   end
 
   def neighbours(x,y)
+    neighbours_count = 0
+
+    if @cells[x - 1][y - 1] && (x != 0 && y != 0) #west 
+      neighbours_count += 1
+    end
+
+    if @cells[x][y - 1] && y != 0 #south
+      neighbours_count += 1
+    end
+
+    if @cells[x + 1][y - 1] && y != 0 #north east 
+      neighbours_count += 1
+    end
+
+    if @cells[x - 1][y] && x != 0 #west
+      neighbours_count
+    end
+
+    if @cells[x + 1][y] && x != 9 #east
+      neighbours_count
+    end
+
+    if @cells[x - 1][y + 1] && (x != 0 && y != 9) #south west
+      neighbours_count
+    end
+    
+    if @cells[x][y + 1] && y != 9 #north
+      neighbours_count
+    end
+   
+    if @cells[x + 1][y + 1] && (x != 9 && y != 9) #east 
+    neighbours_count
+    end
+   
+    return neighbours_count
   end
 
 
 end
 
+class Game
+ attr_accessor :grid
+
+  def initialize # create a new Grid and assigns it to an instance variable @grid. Then it sets some cells live on this grid. 
+   @grid = Grid.new
+   @grid.create_and_set_position
+   @grid.print_board
+  end
+
+  def clear_screen()
+    puts "\e[H\e[2J"
+  end
+
+  def live_in_next_grid?(live, neighbours_count) #returns whether a cell should live or be dead in the next grid
+  # Any live cell with fewer than two live neighbours will be dead in the next grid, as if caused by under-population.
+    if live == true && neighbours_count < 2
+      false
+    elsif live == true && (neighbours_count == 2 || neighbours_count == 3) # Any live cell with two or three live neighbours lives in the next grid.
+      true
+    elsif live == true && neighbours_count > 3 #Any live cell with more than three live neighbours dies, as if by overcrowding.
+      false
+    elsif live == false && neighbours_count == 3 #Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+      true
+    else
+      false
+    end
+  end
+end
+
+
 life = Grid.new
+
 life.play_game
-life.print_board
+puts life.neighbours(3, 6)
+game = Game.new
+
+puts game.live_in_next_grid?(false, 2)
+#game.grid = Grid.new
+#puts game.grid
+#puts game.grid.set_cell(4, 5, true)
+#puts game.grid.live_in_next_grid?(true, 3)
+
 
 
 
--- Game_of_Life_Testing.rb
+++ Game_of_Life_Testing.rb
@@ -5,21 +5,46 @@ require_relative 'Game_of_Life'
 class GridTest < MiniTest::Test
 
 	def setup
-		@grid = Grid.new	
+		@cells = Grid.new	
 	end
 
 	def test_set_cell
-		@grid.set_cell(4, 6, true)
-		@grid.set_cell(5, 7, false)
+		@cells.set_cell(4, 6, true)
+		@cells.set_cell(5, 7, false)
 
-		assert_equal true, @grid.current_cell(4, 6)
-		assert_equal false, @grid.current_cell(5, 7)
+		assert_equal true, @cells.live?(4, 6)
+		assert_equal false, @cells.live?(5, 7)
 	end
 
 	def test_board_as_string
-		@grid.set_cell(0, 0, "X")
-		@grid.set_cell(0, 1, "X")
+		@cells.set_cell(0, 0, "X")
+		@cells.set_cell(1, 0, "X")
 		expected = "XX        \n" + "          \n" * 9
-		assert_equal expected, @grid.board_as_string
+		assert_equal expected, @cells.board_as_string
 	end
+
+	def test_neighbours
+
+		@cells.set_cell(0, 0, true)
+		@cells.set_cell(0, 1, true)
+		@cells.set_cell(1, 2, true)
+
+		assert_equal 0, @cells.neighbours(0, 0)
+		assert_equal 1, @cells.neighbours(0, 1)
+		assert_equal 1, @cells.neighbours(1, 2)
+	end
+
+end
+
+class GameTest < MiniTest::Test
+
+	def setup
+		@game = Game.new
+	end
+
+	def test_live_in_next_grid?
+		assert_equal false, @game.live_in_next_grid?(false, 2)
+		
+	end
+
 end
